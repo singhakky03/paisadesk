fa207a941d59cd3a153c79b364b630ce71202ff3
Ankit Singh <ankit@velvetcase.com>
2017-05-05 16:48:36 +0530

    bkp

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,13 @@ class Category < ActiveRecord::Base
 
   validates :name, presence: true
   TYPE = ["Expense", "Profit"]
+
+  def total(user)
+    Convention.where(user_id: user.id, category_id: self.id).pluck(:amount).sum
+  end
+
+
+
 end
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,20 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
+  def percent_json(categories)
+    convention_amounts =[]
+    categories.where(:category_type => "Expense").each {|cat| exp_amounts << cat.total(self)}
+    categories.where(:category_type => "Profit").each {|cat| exp_amounts << cat.total(self)}
+    proArr = []
+    expArr = []
+    categories.each do |cat|
+      if cat.category_type == "Expense"
+        expArr << {name: cat.name, y:(cat.total(self)/convention_amounts.sum) * 100}
+      else
+        proArr << {name: cat.name, y:(cat.total(self)/convention_amounts.sum) * 100}
+      end
+    end
+    arr = [expArr, proArr]
+  end
+
 end
